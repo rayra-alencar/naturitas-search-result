03c902bfe20f3dfc8c04e17e5135785e0c3fe4e9
Pablo Martin <pmartin@webimpacto.es>
2019-06-07 11:08:30 +0200

    Recomendadores Category y Subcategory

--- react/components/ResultContainer.js
+++ react/components/ResultContainer.js
@@ -1,5 +1,5 @@
 import React, { Component, PureComponent, Fragment } from 'react';
-import {  FormattedMessage } from 'react-intl'
+import { FormattedMessage } from 'react-intl'
 import { graphql } from 'react-apollo'
 
 import categoryNameQuery from '../queries/categoryNameQuery.gql'
@@ -12,7 +12,7 @@ import '../global.css'
 import FilterBlock from './FilterBlock';
 import ToolbarProducts from './ToolbarProducts';
 import ViewMore from './ViewMore';
-import { Spinner } from 'vtex.styleguide'   
+import { Spinner } from 'vtex.styleguide'
 
 const WidthSwithMobileDesktop = 769;
 
@@ -41,17 +41,17 @@ class ResultContainer extends Component {
         super(props)
 
         let titleTag = ''
-        if(props.params.subcategory) titleTag = this.titleTagWithAccent(props.params.subcategory,'subcategory',props.params.department,props.params.category)
-        else if(props.params.category) titleTag = this.titleTagWithAccent(props.params.category,'category',props.params.department,null)
-        else if(props.params.department) titleTag = this.titleTagWithAccent(props.params.department,'department',null,null)
-        else if(props.params.brand) titleTag = this.titleTagWithAccent(props.params.brand,'brands',null,null)
+        if (props.params.subcategory) titleTag = this.titleTagWithAccent(props.params.subcategory, 'subcategory', props.params.department, props.params.category)
+        else if (props.params.category) titleTag = this.titleTagWithAccent(props.params.category, 'category', props.params.department, null)
+        else if (props.params.department) titleTag = this.titleTagWithAccent(props.params.department, 'department', null, null)
+        else if (props.params.brand) titleTag = this.titleTagWithAccent(props.params.brand, 'brands', null, null)
 
         titleTag = titleTag.charAt(0).toUpperCase() + titleTag.slice(1)
 
         this.state = {
             linesDescription: 2,
             titleTag,
-            query: props.query, 
+            query: props.query,
             map: props.map,
             rest: props.rest,
             minPrice: optionsMinPrice[0].value,
@@ -62,117 +62,131 @@ class ResultContainer extends Component {
         }
     }
 
-    static getDerivedStateFromProps(nextProps, prevState){
-        
-        if(nextProps.searchQuery.titleTag && nextProps.searchQuery.titleTag!==prevState.titleTag){
-          return { titleTag: nextProps.searchQuery.titleTag};
-       }
-        
-       else return null;
-     }
+    static getDerivedStateFromProps(nextProps, prevState) {
 
-    shouldComponentUpdate(nextProps){
-        if(nextProps.loading != this.props.loading){
+        if (nextProps.searchQuery.titleTag && nextProps.searchQuery.titleTag !== prevState.titleTag) {
+            return { titleTag: nextProps.searchQuery.titleTag };
+        }
+
+        else return null;
+    }
+
+    shouldComponentUpdate(nextProps) {
+        if (nextProps.loading != this.props.loading) {
             return true;
         }
-       /* TODO -> OPTIMIZAR CONDICION, NO VALE SI EL NUMERO DE PRODUCTOS ES IGUAL
-            if(nextProps.products.length == this.props.products.length){
-            return false;
-        }*/
+        /* TODO -> OPTIMIZAR CONDICION, NO VALE SI EL NUMERO DE PRODUCTOS ES IGUAL
+             if(nextProps.products.length == this.props.products.length){
+             return false;
+         }*/
 
         return true;
     }
 
     updateOrderBy = (orderByField) => {
-        this.props.searchQuery.refetch({orderBy: orderByField})
-        this.setState({orderByField})   
-      }
-    
+        this.props.searchQuery.refetch({ orderBy: orderByField })
+        this.setState({ orderByField })
+    }
+
     updatePrice = (minPrice, maxPrice) => {
         let query = this.state.query;
         let map = this.state.map;
 
         //Si el precio es diferente de 0 e infinito, metemos en la query: de-{min}-a-{max}
-        if(minPrice != optionsMinPrice[0].value || maxPrice != optionsMaxPrice[optionsMaxPrice.length - 1].value){
+        if (minPrice != optionsMinPrice[0].value || maxPrice != optionsMaxPrice[optionsMaxPrice.length - 1].value) {
             query += `/de-${minPrice}-a-${maxPrice}`
             let queryArray = query.split('/')
             let mapArray = map.split(',')
-            mapArray.splice( queryArray.length-1, 0, 'priceFrom')
-            map = mapArray.join(',') 
+            mapArray.splice(queryArray.length - 1, 0, 'priceFrom')
+            map = mapArray.join(',')
         }
 
-        this.props.searchQuery.refetch({query,map})
-        this.setState({minPrice,maxPrice, userInteractiveWithFilters:true})
-      }
-    
+        this.props.searchQuery.refetch({ query, map })
+        this.setState({ minPrice, maxPrice, userInteractiveWithFilters: true })
+    }
+
     updateQuerySearch = (map, rest) => {
-        this.props.searchQuery.refetch({map,rest})
-        this.setState({map,rest, userInteractiveWithFilters:true})
-      }
+        this.props.searchQuery.refetch({ map, rest })
+        this.setState({ map, rest, userInteractiveWithFilters: true })
+    }
 
-    titleTagWithAccent = (title,type,department,category) =>{
-        let result='';
-        let auxDepartment='';
-        let auxCategory='';
-        let auxBrand='';
+    titleTagWithAccent = (title, type, department, category) => {
+        let result = '';
+        let auxDepartment = '';
+        let auxCategory = '';
+        let auxBrand = '';
 
-        if(title){
-            title=title.replace(/%20/g, '-')
-            title=title.replace(/ /g, '-')
-        } 
+        if (title) {
+            title = title.replace(/%20/g, '-')
+            title = title.replace(/ /g, '-')
+        }
 
-        if(department){
-            department=department.replace(/%20/g, '-')
-            department=department.replace(/ /g, '-')
-        } 
+        if (department) {
+            department = department.replace(/%20/g, '-')
+            department = department.replace(/ /g, '-')
+        }
 
-        if(category){
-            category=category.replace(/%20/g, '-')
-            category=category.replace(/ /g, '-')
+        if (category) {
+            category = category.replace(/%20/g, '-')
+            category = category.replace(/ /g, '-')
 
-        } 
-     
-        let categories=this.props.data.categories;
-      
+        }
 
-        if(typeof categories != 'undefined'){
-            if (type == 'department'){
-                result=categories.filter(item =>item.slug == title)
-            }else if (type == 'category'){
-                auxDepartment=categories.filter(item =>item.slug == department)
-                if(auxDepartment.length){
-                    if(auxDepartment[0].children.length) result=auxDepartment[0].children.filter(item =>item.slug == title)
+        let categories = this.props.data.categories;
+
+
+        if (typeof categories != 'undefined') {
+            if (type == 'department') {
+                result = categories.filter(item => item.slug == title)
+            } else if (type == 'category') {
+                auxDepartment = categories.filter(item => item.slug == department)
+                if (auxDepartment.length) {
+                    if (auxDepartment[0].children.length) result = auxDepartment[0].children.filter(item => item.slug == title)
                 }
-            }else if (type == 'subcategory'){
-                
-                auxDepartment=categories.filter(item =>item.slug == department)
-                if(auxDepartment.length){
-                    if(auxDepartment[0].children.length){ 
-                        auxCategory=auxDepartment[0].children.filter(item =>item.slug == category)
-                        if(auxCategory.length){
-                            result=auxCategory[0].children.filter(item =>item.slug == title)
+            } else if (type == 'subcategory') {
+
+                auxDepartment = categories.filter(item => item.slug == department)
+                if (auxDepartment.length) {
+                    if (auxDepartment[0].children.length) {
+                        auxCategory = auxDepartment[0].children.filter(item => item.slug == category)
+                        if (auxCategory.length) {
+                            result = auxCategory[0].children.filter(item => item.slug == title)
                         }
                     }
                 }
-            }else if(type == 'brands'){
-               
-               auxBrand = title.replace(/-/g, ' ')
-               return decodeURI(auxBrand)
+            } else if (type == 'brands') {
+
+                auxBrand = title.replace(/-/g, ' ')
+                return decodeURI(auxBrand)
             }
         }
-        
-       if(result.length){
-        return result[0].name;
-       }else{
-        return '';
-       }
-       
-    }  
+
+        if (result.length) {
+            return result[0].name;
+        } else {
+            return '';
+        }
+
+    }
 
     render() {
         const { searchQuery, notfoundimage, params, map } = this.props
         const { facets } = searchQuery
-        
+
+        let categoryPath = ''
+
+        if(this.props.products && this.props.products[0]){
+            if (this.props.params.subcategory) {
+                categoryPath = this.props.products[0].categories[0]
+            }
+            else if (this.props.params.category) {
+                categoryPath = this.props.products[0].categories[1]
+            }
+            else if (this.props.params.department) {
+                categoryPath = this.props.products[0].categories[2]
+            }
+        }
+
 
         if (!this.state.userInteractiveWithFilters && (!facets || !facets.CategoriesTrees[0]) && !searchQuery.loading) {
             return (
@@ -190,7 +204,7 @@ class ResultContainer extends Component {
 
                                         {mobileMode && (
                                             <Fragment>
-                                                <ExtensionPoint id="whatsapp"/>
+                                                <ExtensionPoint id="whatsapp" />
                                             </Fragment>
                                         )}
 
@@ -250,16 +264,16 @@ class ResultContainer extends Component {
 
                                 {mobileMode &&
                                     <div className="d-flex">
-                                        <FilterBlock mobileMode={mobileMode} updatePrice={this.updatePrice} updateQuerySearch={this.updateQuerySearch} map={map} searchQuery={searchQuery} facets={facets} params={params} optionsMinPrice={optionsMinPrice} optionsMaxPrice={optionsMaxPrice}/>
+                                        <FilterBlock mobileMode={mobileMode} updatePrice={this.updatePrice} updateQuerySearch={this.updateQuerySearch} map={map} searchQuery={searchQuery} facets={facets} params={params} optionsMinPrice={optionsMinPrice} optionsMaxPrice={optionsMaxPrice} />
                                         <ToolbarProducts updateOrderBy={this.updateOrderBy} updatePrice={this.updatePrice} recordsFiltered={searchQuery.recordsFiltered} />
                                     </div>
                                 }
 
                                 <div id="category-main-container" className="container mt-md-3 d-flex flex-wrap">
                                     {!mobileMode &&
-                                        <FilterBlock mobileMode={mobileMode}  updatePrice={this.updatePrice} updateQuerySearch={this.updateQuerySearch} map={map} searchQuery={searchQuery} facets={facets} params={params} optionsMinPrice={optionsMinPrice} optionsMaxPrice={optionsMaxPrice}/>
-                                    }    
-                                    
+                                        <FilterBlock mobileMode={mobileMode} updatePrice={this.updatePrice} updateQuerySearch={this.updateQuerySearch} map={map} searchQuery={searchQuery} facets={facets} params={params} optionsMinPrice={optionsMinPrice} optionsMaxPrice={optionsMaxPrice} />
+                                    }
+
                                     <div id="products-block">
                                         {!mobileMode &&
                                             <Fragment>
@@ -269,24 +283,33 @@ class ResultContainer extends Component {
                                         }
 
 
-                                        {this.state.userInteractiveWithFilters && this.props.products.length<=0
-                                         ? <div className="note-msg">
-                                            <FormattedMessage id="searchresult.noproducts" />
+                                        {this.state.userInteractiveWithFilters && this.props.products.length <= 0
+                                            ? <div className="note-msg">
+                                                <FormattedMessage id="searchresult.noproducts" />
                                             </div>
-                                         : 
-                                          (this.props.products.length==0 && this.props.loading) ? (<div className="d-flex mt-3">  <div className="text-primary mx-auto"> <Spinner color="currentColor"/></div></div>)
-                                          : (<ExtensionPoint
-                                            id="productList"
-                                            products={this.props.products}
-                                            loading={this.props.loading}
-                                            params={this.props.params}
-                                            maxItemsPerPage={this.props.maxItemsPerPage}
-                                            variables={this.props.searchQuery.variables}
-                                          />)
+                                            :
+                                            (this.props.products.length == 0 && this.props.loading) ? (<div className="d-flex mt-3">  <div className="text-primary mx-auto"> <Spinner color="currentColor" /></div></div>)
+                                                : (
+                                                    <Fragment>
+                                                        {categoryPath &&
+                                                            <div data-retailrocket-markup-block="5ceeb37497a52509cc2e1b30" data-
+                                                                category-path={categoryPath}></div>
+                                                        }
+
+                                                        <ExtensionPoint
+                                                            id="productList"
+                                                            products={this.props.products}
+                                                            loading={this.props.loading}
+                                                            params={this.props.params}
+                                                            maxItemsPerPage={this.props.maxItemsPerPage}
+                                                            variables={this.props.searchQuery.variables}
+                                                        />
+                                                    </Fragment>
+                                                )
                                         }
                                         {mobileMode && (
                                             <Fragment>
-                                                <ExtensionPoint id="whatsapp"/>
+                                                <ExtensionPoint id="whatsapp" />
                                             </Fragment>
                                         )}
 
